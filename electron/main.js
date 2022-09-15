43ac3591820e7f78251092baf59a3f71ce7f9124
HWWHEN <1145908327@qq.com>
2022-09-15 10:59:59 +0800

    test update

--- electron/main.js
+++ electron/main.js
@@ -4,22 +4,94 @@ const path = require("path");
 const electron = require("electron");
 const Menu = electron.Menu;
 const NODE_ENV = process.env.NODE_ENV;
+const { autoUpdater } = require("electron-updater");
+const { ipcMain } = require("electron");
 function createWindow() {
   Menu.setApplicationMenu(null); //关闭菜单栏
   // 创建浏览器窗口
   const mainWindow = new BrowserWindow({
-    width: 450,
+    width: 1650,
     height: 800,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
-
   // 加载 index.html
   mainWindow.loadURL(NODE_ENV === "development" ? "http://localhost:3000" : `file://${path.join(__dirname, "../dist/index.html")}`);
   // 打开开发工具
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 }
+
+function checkUpdate() {
+  // 配置提供更新的程序，及build中配置的url
+  autoUpdater.setFeedURL("http://www.mr-hei.tech:88/searchUpdate");
+  // 是否自动更新，如果为true，当可以更新时(update-available)自动执行更新下载。
+  autoUpdater.autoDownload = false;
+
+  // 1. 在渲染进程里触发获取更新，开始进行更新流程。 (根据具体需求)
+  ipcMain.on("checkForUpdates", (e, arg) => {
+    autoUpdater.checkForUpdates();
+  });
+
+  autoUpdater.on("error", function (error) {
+    printUpdaterMessage("error");
+    mainWindow.webContents.send("updateError", error);
+  });
+
+  // 2. 开始检查是否有更新
+  autoUpdater.on("checking-for-update", function () {
+    printUpdaterMessage("checking");
+  });
+
+  // 3. 有更新时触发
+  autoUpdater.on("update-available", function (info) {
+    printUpdaterMessage("updateAvailable");
+    // 4. 告诉渲染进程有更新，info包含新版本信息
+    mainWindow.webContents.send("updateAvailable", info);
+  });
+
+  // 7. 收到确认更新提示，执行下载
+  ipcMain.on("comfirmUpdate", () => {
+    autoUpdater.downloadUpdate();
+  });
+
+  autoUpdater.on("update-not-available", function (info) {
+    printUpdaterMessage("updateNotAvailable");
+  });
+
+  // 8. 下载进度，包含进度百分比、下载速度、已下载字节、总字节等
+  // ps: 调试时，想重复更新，会因为缓存导致该事件不执行，下载直接完成，可找到C:\Users\40551\AppData\Local\xxx-updater\pending下的缓存文件将其删除（这是我本地的路径）
+  autoUpdater.on("download-progress", function (progressObj) {
+    printUpdaterMessage("downloadProgress");
+    mainWindow.webContents.send("downloadProgress", progressObj);
+  });
+
+  // 10. 下载完成，告诉渲染进程，是否立即执行更新安装操作
+  autoUpdater.on("update-downloaded", function () {
+    mainWindow.webContents.send("updateDownloaded");
+    // 12. 立即更新安装
+    ipcMain.on("updateNow", (e, arg) => {
+      autoUpdater.quitAndInstall();
+    });
+  });
+
+  // 将日志在渲染进程里面打印出来
+  function printUpdaterMessage(arg) {
+    let message = {
+      error: "更新出错",
+      checking: "正在检查更新",
+      updateAvailable: "检测到新版本",
+      downloadProgress: "下载中",
+      updateNotAvailable: "无新版本",
+    };
+    mainWindow.webContents.send("printUpdaterMessage", message[arg] ?? arg);
+  }
+}
+
+app.on("ready", () => {
+  //每次启动程序，就检查更新
+  checkUpdate();
+});
 
 // 这段程序将会在 Electron 结束初始化
 // 和创建浏览器窗口的时候调用
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,53 @@
 <script setup>
 import searchAttribute from "./view/search-attribute.vue";
+// import { ref } from "vue";
+// import { ipcRenderer } from "electron";
+// import { ElMessage } from "element-plus";
+// const ifUpdate = ref(false);
+// const updateInfo = ref();
+// const ifUpdateing = ref(false);
+// const ifInstall = ref(false);
+// const downloadInfo = ref();
+// // 5. 收到主进程可更新的消息，做自己的业务逻辑
+// ipcRenderer.on("updateAvailable", (event, data) => {
+//   console.log("updateAvailable:" + data);
+//   ifUpdate.value = true;
+// });
+
+// //打印消息
+// ipcRenderer.on("printUpdaterMessage", (event, data) => {
+//   ElMessage.success(data);
+// });
+
+// // 9. 收到进度信息，做进度条
+// ipcRenderer.on("downloadProgress", (event, data) => {
+//   console.log("downloadInfo:" + data);
+//   downloadInfo.value = data;
+// });
+
+// // 11. 下载完成，反馈给用户是否立即更新
+// ipcRenderer.on("updateDownloaded", (event, data) => {
+//   ifInstall.value = true;
+// });
+
+// // 12. 告诉主进程，立即更新
+// // ipcRenderer.send("updateNow");
+
+// // 6. 点击确认更新
+// function comfirmUpdate() {
+//   ipcRenderer.send("comfirmUpdate"); //发送下载请求
+//   ifUpdate.value = false; //关闭本窗口
+//   updateInfo.value = true; //打开下载进度窗口
+// }
 </script>
 
 <template>
   <searchAttribute />
+  <!-- 提示更新 -->
+  <!-- <el-dialog v-model="ifUpdate" title="版本更新">
+    <el-button type="primary" @click="comfirmUpdate">立即更新</el-button>
+    <el-button @click="ifUpdate = false">忽略更新</el-button>
+  </el-dialog> -->
 </template>
 
 <style scoped></style>
